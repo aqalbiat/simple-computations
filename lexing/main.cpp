d14110fd4b2504ce1ecaf2c6ee483a08c039b112
Aman <qalbiataman@gmail.com>
2026-02-04 12:32:53 +0500

    some refactoring for testing process: adding result mask

--- lexing/main.cpp
+++ lexing/main.cpp
@@ -9,6 +9,15 @@ using namespace std;
 vector <string> test1;
 vector <char> test2;
 
+int build_mask(string ptrn) {
+    int res = 0;
+    for (int i = 0; i < 24; i++) {
+        int digit = (int)(ptrn[i] - '0');
+        res = (res + (digit << i));
+    }
+    return res;
+}
+
 void init_tests() {
     // Test some strings
     test1.push_back("Hello, how are you?");
@@ -21,6 +30,10 @@ void init_tests() {
     test1.push_back("low");
     test1.push_back("stroke key");
     test1.push_back("milestone");
+    test1.push_back("c");
+    test1.push_back("d");
+    test1.push_back("lololo");
+    test1.push_back("cropsfield");
     // Test some characters
     test2.push_back('s');
     test2.push_back('f');
@@ -34,15 +47,27 @@ void init_tests() {
     test2.push_back('a');
 }
 
-void RunTests(Edge e) {
+void RunTests(Edge e, int mask) {
+    int ok_count = 0;
     for (auto curr : test1) {
+        bool need = (mask & 1);
+        mask >>= 1;
+        bool curr_result = e.check_edge(curr); 
         cout << "Testing -> " << curr << "; result = ";
-        cout << e.check_edge(curr) << "\n";
+        cout << curr_result << "\n";
+        cout << "Needed result: " << need << " : " << (need == curr_result ? "OK" : "Bad") << "\n";
+        ok_count += (need == curr_result ? 1 : 0);
     }
     for (auto curr : test2) {
+        bool need = (mask & 1);
+        mask >>= 1;
+        bool curr_result = e.check_edge(curr); 
         cout << "Testing -> " << curr << "; result = ";
-        cout << e.check_edge(curr) << "\n";
+        cout << curr_result << "\n";
+        cout << "Needed result: " << need << " : " << (need == curr_result ? "OK" : "Bad") << "\n";
+        ok_count += (need == curr_result ? 1 : 0);
     }
+    cout << "Number of successful runs: " << ok_count << "/" << test1.size() + test2.size() << "\n";
 }
 
 int main() {
@@ -54,7 +79,8 @@ int main() {
     e.add_clause(Clause('a', 'e'));
     e.add_clause(Clause('j'));
     e.add_clause(Clause('z'));
-    RunTests(e);
+    int curr_mask = build_mask("000111000011000000000011");
+    RunTests(e, curr_mask);
     cout << "\n\n ============= FINISHED ============= \n\n";
     return 0;
 }
